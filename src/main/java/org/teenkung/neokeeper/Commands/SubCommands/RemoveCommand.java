6fc3a0764de50fd35016a3c10478b7ee953c2626
Teenkung123 <suraritnuthaphon@gmail.com>
2024-11-25 23:58:07 +0700

    Fixed RemoveCommand

--- src/main/java/org/teenkung/neokeeper/Commands/SubCommands/RemoveCommand.java
+++ src/main/java/org/teenkung/neokeeper/Commands/SubCommands/RemoveCommand.java
@@ -7,7 +7,7 @@ import org.teenkung.neokeeper.NeoKeeper;
 public class RemoveCommand {
 
     public void execute(NeoKeeper plugin, CommandSender player, String[] args) {
-        if (args.length == 3) {
+        if (args.length == 2) {
             String id = args[1];
             if (plugin.getShopLoader().getAllTradeManagers().containsKey(id)) {
                 plugin.getShopLoader().deleteShop(id, true);
@@ -16,7 +16,7 @@ public class RemoveCommand {
                 player.sendMessage(plugin.colorize("<red>Shop id: " + id + " does not exists!"));
             }
         } else {
-            player.sendMessage(plugin.colorize("<red>Invalid Arguments! usage: /neokeeper edit <shopID>"));
+            player.sendMessage(plugin.colorize("<red>Invalid Arguments! usage: /neokeeper remove <shopID>"));
         }
     }
 
